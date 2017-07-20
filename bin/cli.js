5d7b544821d6e9ac60ac17618f4cda44a066e2c8
James Benner <james.benner@gmail.com>
2017-07-20 16:19:04 -0400

    Adding basic functionality.

--- bin/cli.js
+++ bin/cli.js
@@ -3,9 +3,36 @@
 'use strict';
 
 const app = require('../');
+const {bold} = require('chalk');
+const logSymbols = require('log-symbols');
+const meow = require('meow');
 
-const cwd = process.cwd();
-const pkg = require(`${cwd}/package.json`);
-const readme = app({pkg});
+const cli = meow(`
+    Usage
+      $ readme-md
+
+    Options
+      --help, -h     Display this message.
+      --version, -v  Display the application version.
+`, {
+    alias: {
+        h: 'help',
+        v: 'version'
+    }
+});
+
+let cwd = process.cwd();
+let pkg;
+
+try {
+    pkg = require(`${cwd}/package.json`);
+} catch (_) {
+    let message = `No ${bold('package.json')} file found. Try another directory?`
+
+    console.error(logSymbols.error, message);
+    process.exit(1);
+}
+
+let readme = app({pkg});
 
 console.log(readme);
---
+++ spec/readme-md-cli-spec.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const any = jasmine.any;
+const {execSync} = require('child_process');
+const fs = require('fs-extra');
+const tempy = require('tempy');
