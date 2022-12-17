108635c0b60c94b82f43b74d9d7a30fa567a249e
James Benner <james.benner@gmail.com>
2022-12-17 16:38:39 -0500

    Upgrade husky and lint-staged dependencies

--- .huskyrc.js
+++
@@ -1,7 +0,0 @@
-/** @see https://github.com/typicode/husky#readme */
-module.exports = {
-    hooks: {
-        'pre-commit': 'lint-staged',
-        'pre-push': 'npm test'
-    }
-};
--- .lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-/** @see https://github.com/okonet/lint-staged#readme */
-module.exports = {
-    '*.js': ['npx eslint --fix', 'git add'],
-    'package.json': ['sort-package-json', 'git add']
-};
