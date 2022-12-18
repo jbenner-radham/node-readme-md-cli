df1e16d15a1d29fd7b46f1122b288f80c51761c0
James Benner <james.benner@gmail.com>
2022-12-18 11:30:27 -0500

    Update CI specific test for GitHub Actions

--- spec/readme-md-cli.spec.js
+++ spec/readme-md-cli.spec.js
@@ -6,7 +6,7 @@ import fs from 'node:fs';
 import tempy from 'tempy';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const _describe = process.env.TRAVIS ? describe : xdescribe;
+const _describe = process.env.CI === 'true' ? describe : xdescribe;
 const pkg = JSON.parse(fs.readFileSync(`${process.cwd()}/package.json`).toString());
 const [bin] = Object.keys(pkg.bin);
 const version = pkg.version;
@@ -22,17 +22,19 @@ _describe('readme-md-cli', function () {
         execa.commandSync(`yarn unlink ${pkg.name}`, { shell: true });
     });
 
+    /* eslint-disable max-len */
     beforeEach(function () {
         /**
-         * Sometimes the builds run slow on Travis-CI so crank up the timeout
-         * just in case.
+         * We had to do this on Travis-CI so I'm keeping it like this just in
+         * case. I might revisit this choice later.
          *
-         * @see https://docs.travis-ci.com/user/environment-variables#Default-Environment-Variables
+         * @see https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
          */
-        if (process.env.TRAVIS === 'true') {
+        if (process.env.CI === 'true') {
             jasmine.DEFAULT_TIMEOUT_INTERVAL = 25000;
         }
     });
+    /* eslint-enable max-len */
 
     it('is an available system command', function () {
         expect(commandExistsSync(bin)).toBe(true);
